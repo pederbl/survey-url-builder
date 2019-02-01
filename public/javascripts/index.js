874d68ccf09cc8a0dde50d2840e49695f00a6b08
Peder Linder <pederbl@gmail.com>
2019-02-01 15:01:23 +0100

    Minor update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,6 @@ var base_url;
 var params_to_set;
 var fixed_params;
 
-
 function toQueryString(obj) {
   var str = [];
   for (var p in obj)
@@ -13,19 +12,35 @@ function toQueryString(obj) {
 }
 
 window.onload = function() { 
-  var url_div = document.getElementById('url'); 
+  var url_element = document.getElementById('url'); 
 
   function setURL() {
     var params = JSON.parse(JSON.stringify(fixed_params));
     params_to_set.forEach(function(item, index) {
       params[item] = document.getElementById('param_to_set_' + item).value;
     });
-    url_div.innerText = base_url + "?" + toQueryString(params);
+    var url = base_url + "?" + toQueryString(params);
+    url_element.innerText = url;
+  }
+
+  function copyURL(obj) {
+    var range = document.createRange();
+    range.selectNode(url_element);
+    window.getSelection().addRange(range);
+    try {
+      document.execCommand('copy');
+    } catch(err) {
+      alert("Copy to clipboard was not successful.")
+    }
+
+    window.getSelection().removeAllRanges();
   }
 
   params_to_set.forEach(function(item, index) {
     document.getElementById('param_to_set_' + item).addEventListener("keyup", setURL);
   });
+
+  document.getElementById('copyURL').addEventListener('click', copyURL);
 
   setURL();
 }
