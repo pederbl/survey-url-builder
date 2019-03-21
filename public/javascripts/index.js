f520fc8986e6632b67aa08a57dd2e738e45ba424
Peder Linder <pederbl@gmail.com>
2019-03-21 12:05:25 +0100

    short

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,8 @@ var base_url;
 var params_to_set;
 var fixed_params;
 
+var shortUrl = require('node-url-shortener');
+
 function toQueryString(obj) {
   var str = [];
   for (var p in obj)
@@ -11,26 +13,31 @@ function toQueryString(obj) {
   return str.join("&");
 }
 
-function getURL() {
-    var params = JSON.parse(JSON.stringify(fixed_params));
-    params_to_set.forEach(function(item, index) {
-      params[item] = document.getElementById('param_to_set_' + item).value;
-    });
-    var url = base_url + "?" + toQueryString(params);
-    console.log(url);
-    return url;
+function getUrl() {
+  var params = JSON.parse(JSON.stringify(fixed_params));
+  params_to_set.forEach(function(item, index) {
+    params[item] = document.getElementById('param_to_set_' + item).value;
+  });
+  var url = base_url + "?" + toQueryString(params);
+  return url;
+}
+
+function getShortUrl(longUrl, callback) {
+  shortUrl.short('https://google.com', function(err, shortUrl) {
+    callback(shortUrl);
+  });
 }
 
 window.onload = function() { 
   var url_element = document.getElementById('url');
 
-  function setURL() {
-    var url = getURL();
+  function setUrl() {
+    var url = getUrl();
     url_element.innerText = url;
     url_element.setAttribute('href', url);
   }
 
-  function copyURL(obj) {
+  function copyUrl(obj) {
     var selection = window.getSelection();
     var range = document.createRange();
     range.selectNodeContents(url_element);
@@ -41,28 +48,30 @@ window.onload = function() {
     } catch(err) {
       alert("Copy to clipboard was not successful.")
     }
-
     window.getSelection().removeAllRanges();
   }
 
   function sendSMS(obj) {
-    var url = 'sms:' + document.getElementById('param_to_set_phone').value.replace(/[^0-9.]/g, "") + "?&body=" + encodeURIComponent(getURL());
+    var url = 'sms:' + document.getElementById('param_to_set_phone').value.replace(/[^0-9.]/g, "") + "?&body=" + encodeURIComponent(getUrl());
     window.open(url);
   }
 
   function sendEmail(obj) {
-    var url = 'mailto:' + document.getElementById('param_to_set_email').value + "?body=" + encodeURIComponent(getURL());
-    window.open(url);
+    longUrl = getUrl();
+    getShortUrl(longUrl, function(shortUrl) {
+      var url = 'mailto:' + document.getElementById('param_to_set_email').value + "?body=" + encodeURIComponent(shortUrl);
+      window.open(url);
+    }
   }
 
   params_to_set.forEach(function(item, index) {
-    document.getElementById('param_to_set_' + item).addEventListener("keyup", setURL);
+    document.getElementById('param_to_set_' + item).addEventListener("keyup", setUrl);
   });
-  document.getElementById('copyURL').addEventListener('click', copyURL);
+  document.getElementById('copyUrl').addEventListener('click', copyUrl);
   document.getElementById('sendSMS').addEventListener('click', sendSMS);
   document.getElementById('sendEmail').addEventListener('click', sendEmail);
 
-  setURL();
+  setUrl();
 }
 
 
